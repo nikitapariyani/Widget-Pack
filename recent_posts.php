8111c994392ca04e983f0a4bbaaf64bf70eb9201
nikitapariyani <pariyaninikita@gmail.com>
2014-08-21 11:54:07 +0530

    Edited to correct form function

--- recent_posts.php
+++ recent_posts.php
@@ -125,10 +125,6 @@ class wp_recent_posts extends WP_Widget {
             <label for="<?php echo $this->get_field_id('post_count'); ?>"><?php _e('Number of Posts', 'wp_widget_plugin'); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id('post_count'); ?>" name="<?php echo $this->get_field_name('post_count'); ?>" type="number" value="<?php echo $post_count;?>">
 	</p>
-        <p>
-            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-        </p>
     <?php
     }
 	
